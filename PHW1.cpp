d66900f60d2c71679a594f00a12e1db83f7c41fe
40312358002 <Rania.Afsharian@gmail.com>
2025-10-24 20:10:56 +0330

    Add function

--- PHW1.cpp
+++ PHW1.cpp
@@ -13,3 +13,30 @@ struct Student
     string securityCode;
     Student *next;
 };
+
+void addstudent(Student *&head, const string &name, int studentID, int units, float gpa, const string &securityCode)
+{
+    Student *newStudent = new Student;
+    newStudent->name = name;
+    newStudent->studentID = studentID;
+    newStudent->units = units;
+    newStudent->gpa = gpa;
+    newStudent->securityCode = securityCode;
+    newStudent->next = nullptr;
+
+    if (head == nullptr)
+    {
+        head = newStudent;
+    }
+    else
+    {
+        Student *temp = head;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+        temp->next = newStudent;
+    }
+
+    cout << "New student added successfully." << endl;
+};
